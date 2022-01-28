e7177f0b586ab04147207345cb7879e53d1bddf9
kmavillanosa <kmavillanosa@gmail.com>
2022-01-28 12:21:39 +0800

    modified firstname datatype

--- CodeFirst.NetCore/Models/User.cs
+++ CodeFirst.NetCore/Models/User.cs
@@ -8,7 +8,7 @@ namespace CodeFirst.NetCore.Models
     public class User
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public int UserGroupId { get; set; }
         public DateTime CreationDateTime { get; set; }
