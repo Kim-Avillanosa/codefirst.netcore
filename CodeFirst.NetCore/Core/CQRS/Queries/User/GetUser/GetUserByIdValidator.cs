c167e4426ba5a9abd7f03ce9f45dc98b23328726
kmavillanosa <kmavillanosa@gmail.com>
2022-02-01 14:14:52 +0800

    Added query validators

---
+++ CodeFirst.NetCore/Core/CQRS/Queries/User/GetUser/GetUserByIdValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst.NetCore
+{
+    public class GetUserByIdValidator : AbstractValidator<GetUserByIdQuery>
+    {
+        public GetUserByIdValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+        }
+    }
+}
